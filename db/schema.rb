76a746a44c8ac2b986d1507e5017f93ce65ac47d
David H <dhovanky@gmail.com>
2016-02-24 01:36:52 +0000

    added micropost model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218010104) do
+ActiveRecord::Schema.define(version: 20160223071343) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +28,8 @@ ActiveRecord::Schema.define(version: 20160218010104) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "city"
+    t.string   "profile"
   end
 
 end
