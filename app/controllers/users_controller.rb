50c634ed5c6fbe117749b4c19649cc57fbd15a8d
David H <dhovanky@gmail.com>
2016-02-25 04:55:43 +0000

    fixed validation and authentication bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  
   def new
     @user = User.new
   end
@@ -11,30 +12,53 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Thank you for joining Microposts!"
       redirect_to @user
     else
       render 'new'
    end
   end
   
-  def edit
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_profile)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'プロフィールを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit', flash: { danger: "error" }
+    end
   end
   
-  def update 
+  def edit
     @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to @user , notice: 'プロフィールを編集しました'
-    else
-      render 'edit'
+    if @user != current_user
+      redirect_to current_user, flash: { danger: "不正なアクセス" + " Logged in as: " + current_user.name}
     end
   end
   
   private
-  
-  def user_params 
-    params.require(:user).permit(:name, :email, :password, :profile, :city,
-                                 :password_confirmation)
+
+  # get parameters for sign up
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-                               
+
+  # get parameters for profile editing
+  def user_profile
+    params.require(:user).permit(:name, :email, :profile, :city, :password, :password_confirmation)
+  end
+
+  # get a user instance with :id parameter
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # check current_user is editing self ?
+  def authenticate!
+    if @user != current_user
+      redirect_to root_url, flash: { danger: "不正なアクセス" }
+    end
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
     before_save { self.email = email.downcase }
     validates :name, presence: true, length: { maximum: 50 }
-    validates :city, presence: false, length: {maximum: 50}
-    validates :profile, presence: false, length: {maximum: 120 }
+    validates :city, presence: true, length: {maximum: 50}
+    validates :profile, presence: true, length: {maximum: 120 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
