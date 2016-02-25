5e1f5243602ca90f9c7c24f82e6f5289bc88a6e4
David H <dhovanky@gmail.com>
2016-02-25 12:50:54 +0000

    added posting micropost functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user, :authenticate!, only: [:edit, :update] 
    def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
   end
   
   def new
