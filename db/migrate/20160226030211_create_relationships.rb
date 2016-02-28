719f68374e42eee18aedb283ca2993b1e880c357
David H <dhovanky@gmail.com>
2016-02-28 07:43:44 +0000

    add followings/followers functionality

---
+++ db/migrate/20160226030211_create_relationships.rb
@@ -0,0 +1,11 @@
+class CreateRelationships < ActiveRecord::Migration
+  def change
+    create_table :relationships do |t|
+      t.references :follower, index: true, foreign_key: true # may need to delete this line
+      t.references :followed, index: true, foreign_key: true # may need to delete this line 
+
+      t.timestamps null: false
+      t.index [:follower_id, :followed_id], unique: true # この行を追加
+    end
+  end
+end
