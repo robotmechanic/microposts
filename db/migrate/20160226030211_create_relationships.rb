e80248b4e3585bf949731f9a7935c624b04393f4
David H <dhovanky@gmail.com>
2016-03-04 03:56:05 +0000

    bug fix

--- db/migrate/20160226030211_create_relationships.rb
+++ db/migrate/20160226030211_create_relationships.rb
@@ -1,8 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration
   def change
     create_table :relationships do |t|
-      #t.references :follower, index: true, foreign_key: true # may need to delete this line
-      #t.references :followed, index: true, foreign_key: true # may need to delete this line 
+      t.references :follower, index: true#, foreign_key: true # may need to delete this line
+      t.references :followed, index: true#, foreign_key: true # may need to delete this line 
 
       t.timestamps null: false
       t.index [:follower_id, :followed_id], unique: true # この行を追加
