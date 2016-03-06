4dd0eeed1727ea9dc769b6206885c3ac68d4e2fa
David H <dhovanky@gmail.com>
2016-03-06 08:41:51 +0000

    fixed issue with text blocking link info

--- app/helpers/thumbnails_helper.rb
+++ app/helpers/thumbnails_helper.rb
@@ -1,16 +1,13 @@
 module ThumbnailsHelper
   def thumbnail_for(micropost_link)
       @link = URI.extract(micropost_link)
-      if micropost_link =~ /\A#{URI::regexp}\z/
+      #if micropost_link =~ /\A#{URI::regexp}\z/
         begin
-        @thumbnail = LinkThumbnailer.generate(micropost_link)
-        rescue LinkThumbnailer::Exceptions => e
-        # do something
+        
+        @thumbnail = LinkThumbnailer.generate(@link[0])
+        #@thumbnail = LinkThumbnailer.generate('http://bandai.com')
+        rescue Exception => e
+            @error = e
         end
-      else
-          if @link.empty? && @link[0] =~ /\A#{URI::regexp}\z/
-            @thumbnail = LinkThumbnailer.generate(@link[0])
-          end
-      end
   end   
 end
